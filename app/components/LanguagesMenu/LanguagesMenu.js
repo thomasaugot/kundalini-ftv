c437122a5aaa6b27d0dba8c8b9fc9229d7064965
Thomas Augot <thomas.augot@hotmail.fr>
2023-11-28 13:59:32 +0100

    set up files, adding navbar & language menu

---
+++ app/components/LanguagesMenu/LanguagesMenu.js
@@ -0,0 +1,68 @@
+"use client";
+
+import React, { useState } from "react";
+import englishFlag from "../../../public/assets/en.png";
+import frenchFlag from "../../../public/assets/fr.png";
+import spanishFlag from "../../../public/assets/es.png";
+import { useTranslation } from "react-i18next";
+import { RxChevronDown } from "react-icons/rx";
+import "./LanguagesMenu.scss";
+import { IconContext } from "react-icons";
+import Image from "next/image";
+
+function LanguagesMenu() {
+  const { i18n } = useTranslation();
+  const [activeLanguage, setActiveLanguage] = useState(i18n.language);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const changeLanguage = (lng) => {
+    setActiveLanguage(lng);
+    setIsMenuOpen(false);
+    i18n.changeLanguage(lng);
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className="languages-menu" onClick={toggleMenu}>
+      <Image
+        src={
+          activeLanguage === "fr" ? frenchFlag : activeLanguage === "en" ? englishFlag : spanishFlag
+        }
+        className="active-flag"
+        alt="Flag"
+        onClick={toggleMenu}
+      />
+      <IconContext.Provider
+        value={{ style: { verticalAlign: "middle", width: "30px", cursor: "pointer" } }}
+        className="chevron-icon"
+      >
+        <RxChevronDown size={20} />
+      </IconContext.Provider>
+
+      <div
+        className="dropdown-menu"
+        style={{
+          display: isMenuOpen ? "block" : "none",
+        }}
+      >
+        <button onClick={() => changeLanguage("fr")} className="flag-button item">
+          <Image src={frenchFlag} alt="French Flag" className="flag-button" />
+          <p>Français</p>
+        </button>
+        <button onClick={() => changeLanguage("en")} className="flag-button item">
+          <Image src={englishFlag} alt="English Flag" className="flag-button" />
+          <p>English</p>
+        </button>
+        <button onClick={() => changeLanguage("es")} className="flag-button item">
+          <Image src={spanishFlag} alt="Spanish Flag" className="flag-button" />
+          <p>Español</p>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default LanguagesMenu;
---
+++ app/page.js
@@ -0,0 +1,11 @@
+// this page is the home page of the app
+
+import "./page.scss";
+
+export default function Home() {
+  return (
+    <main className="main">
+      <div className="home__section">{/* <Image /> */}</div>
+    </main>
+  );
+}
