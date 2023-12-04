afebcbdedce0e14a71d41533171836e2af2d9d49
Thomas Augot <thomas.augot@hotmail.fr>
2023-12-04 13:09:41 +0100

    fixing Image style

--- app/page.js
+++ app/page.js
@@ -1,11 +1,15 @@
 // this page is the home page of the app
 
+import Image from "next/image";
+import lobosIsland from "../public/assets/Home/lobos-island-bg.svg";
 import "./page.scss";
 
 export default function Home() {
   return (
     <main className="main">
-      <div className="home__section">{/* <Image /> */}</div>
+      <div className="home__section">
+        <Image src={lobosIsland} alt="lobos island" width={"100vw"} height={"auto"} />
+      </div>
     </main>
   );
 }
